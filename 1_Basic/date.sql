4692fb1798eec24656ac9ba92a71e55ab36f92ec
iLewisBrogan <06016658@uhi.ac.uk>
2022-04-19 01:17:25 +0100

    renamed folder structure

---
+++ 1_Basic/date.sql
@@ -0,0 +1,16 @@
+-- https://pgexercises.com/questions/basic/date.html
+
+-- How can you produce a list of members who joined after the start of September 2012? 
+-- Return the memid, surname, firstname, and joindate of the members in question.
+
+with base as (
+select
+  memid
+, surname
+, firstname
+, joindate
+from cd.members
+where joindate > '2012-09-01'
+)
+
+select * from base;
